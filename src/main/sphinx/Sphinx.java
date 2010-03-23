4d2ecf629ac9830723fe5cf881024dc47c0e2deb
sbadame <s.badame@gmail.com>
2010-03-23 16:11:35 -0400

    Added sphinx libraries and helloworld to the client

---
+++ src/main/sphinx/Sphinx.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package main.sphinx;
+
+/**
+ * @author deniz
+ *
+ */
+public class Sphinx {
+
+	public Sphinx(){
+		
+	}
+	
+	public String listen(){
+		return null;
+		
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
