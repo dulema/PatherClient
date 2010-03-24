997bb55bef046bc5485500e7d909e6502c29c2af
Deniz Ulema <deniz.ulema@gmail.com>
2010-03-23 23:30:41 -0400

    did up sphinx library

--- src/main/sphinx/Sphinx.java
+++ src/main/sphinx/Sphinx.java
@@ -3,19 +3,43 @@
  */
 package main.sphinx;
 
+import edu.cmu.sphinx.frontend.util.Microphone;
+import edu.cmu.sphinx.recognizer.Recognizer;
+import edu.cmu.sphinx.result.Result;
+import edu.cmu.sphinx.util.props.ConfigurationManager;
+
 /**
- * @author deniz
+ * @author Deniz Ulema
  *
  */
 public class Sphinx {
 
+	Recognizer recognizer;
+	boolean micworks = true;
 	public Sphinx(){
-		
+        System.out.println("Do work0.");
+        ConfigurationManager cm;
+        cm = new ConfigurationManager(Sphinx.class.getResource("helloworld.config.xml"));
+
+        recognizer = (Recognizer) cm.lookup("recognizer");
+        recognizer.allocate();
+
+        Microphone microphone = (Microphone) cm.lookup("microphone");
+        if (!microphone.startRecording()) {
+            recognizer.deallocate();
+            micworks = false;
+            //deactivate voice recog mode here
+        }
+        System.out.println("Do work.");
 	}
 	
 	public String listen(){
-		return null;
-		
+		Result result = recognizer.recognize();
+		return result.getBestFinalResultNoFiller();
+	}
+	
+	public boolean isMicWorking(){
+		return micworks;
 	}
 	
 	/**
