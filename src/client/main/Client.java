de7867c875fb96fac4c87d78ba91af4e154f1b0b
sbadame <s.badame@gmail.com>
2010-03-27 13:29:58 -0400

    No idea why, but the daemon is acting like a child process all of the fricking time... no matter what, there must be an oversight by me

--- src/client/main/Client.java
+++ src/client/main/Client.java
@@ -1,5 +1,9 @@
 package main;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import main.ui.ClientFrame;
 import main.ui.ConnectionDialog;
 import javax.swing.JOptionPane;
@@ -17,24 +21,44 @@ public class Client {
 
     public Client(){
         try {
-            //Just see if the daemon is already running
+            //See if the daemon is already running
             daemon = new Daemon();
         } catch (Exception ex) {
-            ConnectionDialog d;
-            do{
-                d = new ConnectionDialog();
-                if (d.showDialog() == false) {
-                    System.err.println("User canceled");
+            //Try to start the daemon our selves
+            String basedir = Client.class.getResource("").getFile();
+            File filebase = new File(new File(basedir).getParentFile().getParentFile(), "daemon");
+            System.out.println(filebase);
+            try {
+                Process p = Runtime.getRuntime().exec(new String[]{"/bin/bash", "daemon-starter.sh"},
+                                          new String[]{},
+                                          filebase);
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException ex1) {
+                }
+                daemon = new Daemon();
+            } catch (IOException pe){
+                pe.printStackTrace();
+                ConnectionDialog d;
+                do{
+                    d = new ConnectionDialog();
+                    if (d.showDialog() == false) {
+                        System.err.println("User canceled");
+                        System.exit(0);
+                    }
+                }while(d.getPort() == -1 || d.getAddress().isEmpty()); //Keep trying until we get it
+
+                try {
+                    daemon = new Daemon(d.getAddress(), d.getPort());
+                } catch (Exception ce) {
+                    JOptionPane.showMessageDialog(null, "Couldn't connect to daemon");
                     System.exit(0);
                 }
-            }while(d.getPort() == -1 || d.getAddress().isEmpty()); //Keep trying until we get it
 
-            try {
-                daemon = new Daemon(d.getAddress(), d.getPort());
-            } catch (Exception ex1) {
-                JOptionPane.showMessageDialog(null, "Couldn't connect to daemon");
-                System.exit(0);
             }
+
+
+            
         }
 
         frame = new ClientFrame(this);
